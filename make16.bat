087d104a186961adcc9bf930c0d64827e55a1cb6
Valentin Kolesnikov <javadev75@gmail.com>
2012-06-13 09:28:58 +0300

    Improve 16-bit version

--- make16.bat
+++ make16.bat
@@ -1,5 +1,4 @@
-bin\BCC.EXE -IINCLUDE;src\main\c -ms -f- -d -vi- -wpro -weas -wpre -LLIB src/main/c/SILLYAV.C src/main/c/findf.c
-bin\BCC.EXE -IINCLUDE;src\main\c -ms -f- -d -vi- -wpro -weas -wpre -LLIB src/main/c/sillyBAS.c src/main/c/sillylnk.c
+bin\BCC.EXE -IINCLUDE;src\main\c -ms -f- -d -vi- -wpro -weas -wpre -LLIB src/main/c/sillyav.c src/main/c/findf.c
+bin\BCC.EXE -IINCLUDE;src\main\c -ms -f- -d -vi- -wpro -weas -wpre -LLIB src/main/c/sillybas.c src/main/c/sillylnk.c
 bin\BCC.EXE -IINCLUDE;src\main\c -ms -f- -d -vi- -wpro -weas -wpre -LLIB src/main/c/sillytst.c
 bin\MAKE.EXE /f SILLAV16.MAK
-

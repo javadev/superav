2722b78813ac0b5c46e1c2dd715edccc24985db5
Valentyn Kolesnikov <valentyn.kolesnikov@kruschecompany.com>
2013-02-25 10:31:31 +0200

    Improved java version

--- src/main/java/com/github/superav/Superav.java
+++ src/main/java/com/github/superav/Superav.java
@@ -4,18 +4,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Superav {
-public static void main(String[] args) throws Exception {
-    String startMes =
+    public static void main(String[] args) throws Exception {
+        String startMes =
    "\n ----------------------------------------- \n"
  + "|  Super Antivirus for DOS16/WIN32/Java  ||\n"
  + "|         Version 1.0 build 023          ||  javadev75@gmail.com\n"
  + " ========================================= \n\n";
 
-//    memset(BU,0,sizeof(BUFFER));
-
-    java.io.PrintStream ps = new java.io.PrintStream(System.out, true, "UTF-8");
-    ps.println(startMes);
-    Logger.getLogger(Superav.class.getName()).log(Level.INFO, startMes);
-
-}
+        Logger.getLogger(Superav.class.getName()).log(Level.INFO, startMes);
+    }
 }

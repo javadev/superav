ce1c1e425d4c81f652c729a6243a7dc3374c511a
Valentyn Kolesnikov <valentyn.kolesnikov@kruschecompany.com>
2013-02-28 09:38:32 +0200

    Added support for abstract class

---
+++ src/main/java/com/github/superav/AbstractSuperav.java
@@ -0,0 +1,8 @@
+package com.github.superav;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public abstract class AbstractSuperav {
+    protected boolean flagAllFiles;
+}
--- src/main/java/com/github/superav/Superav.java
+++ src/main/java/com/github/superav/Superav.java
@@ -3,8 +3,9 @@ package com.github.superav;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Superav {
+public class Superav extends AbstractSuperav {
     public static void main(String[] args) throws Exception {
+        Superav superav = new Superav();
         String startMes =
    "\n ----------------------------------------- \n"
  + "|  Super Antivirus for DOS16/WIN32/Java  ||\n"
@@ -12,5 +13,14 @@ public class Superav {
  + " ========================================= \n\n";
 
         Logger.getLogger(Superav.class.getName()).log(Level.INFO, startMes);
+        for (String arg : args) {
+            if (arg.startsWith("/") || arg.startsWith("-")) {
+                String pKey = arg.substring(1);
+                if (pKey.equals("*")) {
+                    superav.flagAllFiles = true;
+                    continue;
+                }
+            }
+        }
     }
 }

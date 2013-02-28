124cd475b1c57ab96e32e2761e2789370ce9cb05
Valentyn Kolesnikov <valentyn.kolesnikov@kruschecompany.com>
2013-02-28 10:11:56 +0200

    Added support for logging

--- src/main/java/com/github/superav/AbstractSuperav.java
+++ src/main/java/com/github/superav/AbstractSuperav.java
@@ -5,4 +5,8 @@ import java.util.logging.Logger;
 
 public abstract class AbstractSuperav {
     protected boolean flagAllFiles;
+
+    public static void printf(String message) {
+        Log.info(message);
+    }
 }
--- src/main/java/com/github/superav/Superav.java
+++ src/main/java/com/github/superav/Superav.java
@@ -10,9 +10,9 @@ public class Superav extends AbstractSuperav {
    "\n ----------------------------------------- \n"
  + "|  Super Antivirus for DOS16/WIN32/Java  ||\n"
  + "|         Version 1.0 build 023          ||  javadev75@gmail.com\n"
- + " ========================================= \n\n";
+ + " ========================================= \n";
 
-        Logger.getLogger(Superav.class.getName()).log(Level.INFO, startMes);
+        printf(startMes);
         for (String arg : args) {
             if (arg.startsWith("/") || arg.startsWith("-")) {
                 String pKey = arg.substring(1);
@@ -21,6 +21,21 @@ public class Superav extends AbstractSuperav {
                     continue;
                 }
             }
+        }
+        if (args.length < 2)  {
+            printf("\nUsage: java -jar superav.jar Fname|Path /Keys\n"
+                 + "    /*  scan all files\n"
+                 + "    /-  disinfect\n"
+                 + "    /E  delete infected files\n"
+                 + "    /L  make virus list\n"
+                 + "    /O  display OK messages\n"
+                 + "    /R  do not scan subdirectories\n"
+                 + "    /B  do not scan sectors (32-bit by default)\n"
+                 + "    /M  do not scan memory (32-bit by default)\n"
+                 + "    /V  enable redundant scanning\n"
+                 + "    /W[=filename] save report\n"
+                 + "    /Z  disable aborting\n"
+                 + "    /P  save pages\n");
         }
     }
 }

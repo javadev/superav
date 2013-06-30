6cd4200e0f215f65b82813654edcdfce648632ce
javadev <javadev75@gmail.com>
2013-06-30 20:35:03 +0300

    Improved command line support

--- src/main/java/com/github/superav/Superav.java
+++ src/main/java/com/github/superav/Superav.java
@@ -13,17 +13,17 @@ public class Superav extends AbstractSuperav {
 
         printf(startMes);
         for (String arg : args) {
-            if (arg.startsWith("/") || arg.startsWith("-")) {
-                String pKey = arg.substring(1);
+            if (arg.startsWith("/") || arg.startsWith("--")) {
+                String pKey = arg.substring(arg.startsWith("--") ? 2 : 1);
                 if (pKey.equals("*")) {
                     superav.flagAllFiles = true;
                     continue;
                 }
             }
         }
-        if (args.length >= 2)  {
+        if (args.length >= 1)  {
             for (String arg : args) {
-                if (!arg.startsWith("/") && !arg.startsWith("-")) {
+                if (!arg.startsWith("/") && !arg.startsWith("--")) {
                     printf(String.format("\nProcessing %s\n", arg));
                     new Findf().scanPath(new File(arg), new Findf.Visitor() {
                         public void checkFile(File file) {
@@ -34,18 +34,18 @@ public class Superav extends AbstractSuperav {
             }
         } else {
             printf("\nUsage: java -jar superav.jar Fname|Path /Keys\n"
-                 + "    /*  scan all files\n"
-                 + "    /-  disinfect\n"
-                 + "    /E  delete infected files\n"
-                 + "    /L  make virus list\n"
-                 + "    /O  display OK messages\n"
-                 + "    /R  do not scan subdirectories\n"
-                 + "    /B  do not scan sectors (32-bit by default)\n"
-                 + "    /M  do not scan memory (32-bit by default)\n"
-                 + "    /V  enable redundant scanning\n"
-                 + "    /W[=filename] save report\n"
-                 + "    /Z  disable aborting\n"
-                 + "    /P  save pages\n");
+                 + "    --*  scan all files\n"
+                 + "    ---  disinfect\n"
+                 + "    --E  delete infected files\n"
+                 + "    --L  make virus list\n"
+                 + "    --O  display OK messages\n"
+                 + "    --R  do not scan subdirectories\n"
+                 + "    --B  do not scan sectors (32-bit by default)\n"
+                 + "    --M  do not scan memory (32-bit by default)\n"
+                 + "    --V  enable redundant scanning\n"
+                 + "    --W[=filename] save report\n"
+                 + "    --Z  disable aborting\n"
+                 + "    --P  save pages\n");
         }
 
     }
